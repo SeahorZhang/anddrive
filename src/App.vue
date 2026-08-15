c6ae6e071b256f0e6a25250be232b872f4cde0df
xh <im0k@outlook.com>
2026-08-15 14:35:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup vapor>
+import MainPage from './components/MainPage.vue';
+</script>
+
+<template>
+  <MainPage></MainPage>
+</template>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,4 @@
+<script setup vapor></script>
+<template>
+  <div class="bg-pink-50 px-23">2323</div>
+</template>
